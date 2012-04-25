a0dcd9ca52fe998f1a14d810d68eee484d60cf7b
fangquan <fangquans@gmail.com>
2012-04-25 15:28:27 -0400

    Revert "Source code, V1.0"
    This reverts commit 3df3dacc026dbf45ed815cd8c9d968f6d23396a3.

--- scripts(April25)/test.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bsub -q all_12h  -N -oo ../Chelonia_mydas/GC_cli2.log perl ./GC_cli.pl -M ../Chelonia_mydas/RepeatMasker/MasterFile.out -O ../Chelonia_mydas/Chelonia_GC2 -F ../Chelonia_mydas/GRE.gapCloser.fa.fill.fill.fa
